3f4fea82e7b650adf8885dca0557315da25d5932
AdamZacharzewski <p220063@campus.fct.unl.pt>
2026-09-20 16:17:46 +0200

    test123

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    //sssdss
     public static final String GET_NAME_BY_PHONE = "GN";
 
     //Constantes que definem as mensagens para o utilizador
